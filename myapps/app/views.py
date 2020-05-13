c649cd9100b4db4e2c4bcb9f492a0f432f683ff8
Aniket Aryamane <aryamane.aniket@gmail.com>
2020-05-13 10:47:37 +0530

    Switching to View class

--- myapps/app/views.py
+++ myapps/app/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import View
 
 
 # Create your views here.
-class AjaxHandlerView(TemplateView):
-    template_name = 'app/index.html'
+class AjaxHandlerView(View):
+    def get(self, request):
+        return render(request, 'app/index.html')
