c992d3c7d0be685086956ebe69bf109ca127602e
Aniket Aryamane <aryamane.aniket@gmail.com>
2020-05-13 12:46:50 +0530

    GET request

--- myapps/app/views.py
+++ myapps/app/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.views.generic import View
+from django.http import JsonResponse
+from time import time
 
 
 # Create your views here.
 class AjaxHandlerView(View):
     def get(self, request):
+        text = request.GET.get('request_text')
+        t = time()
+        if request.is_ajax():
+            return JsonResponse({'response_text': str(t)}, status=200)
         return render(request, 'app/index.html')
--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,27 @@
-window.setTimeout(function() {
+window.setTimeout(function () {
   $(".alert")
     .fadeTo(500, 0)
-    .slideUp(500, function() {
+    .slideUp(500, function () {
       $(this).remove();
     });
 }, 1000);
+
+$(document).ready(function () {
+  $(".btn").click(function (e) {
+    e.preventDefault();
+    $.ajax({
+      url: "",
+      type: "GET",
+      data: {
+        request_text: $(this).text(),
+      },
+      dataType: "json",
+      success: function (response) {
+        $(".btn").text(response.response_text);
+        $("#seconds").append(
+          '<li class="list-group-item">' + response.response_text + "</li>"
+        );
+      },
+    });
+  });
+});
